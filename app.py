b637e277d56da4e5333f8f04f4bf0baad1e621d1
jiujiuxr <xrzhou026@gmail.com>
2025-03-07 12:31:27 +0000

    1

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app =Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["POST","GET"])
+def main():
+    user_name =request.form.get("q")
+    return(render_template("main.html"))
+
+@app.route("/ethical_test",methods=["POST","GET"])
+def ethical_test():
+    return(render_template("ethical_test.html"))
+
 if __name__=="__main__":
     app.run()
 
